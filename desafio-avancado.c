b9ec6bff06e3a365850bac82f9c4247d14c91fb9
Viny ✦ <107391217+1SViny@users.noreply.github.com>
2025-11-13 19:22:05 +0000

    Add modularização

--- desafio-avancado.c
+++ desafio-avancado.c
@@ -9,77 +9,60 @@ typedef struct {
     int tropas;
 } Territorio;
 
-// Função para entrada de territórios
+// Função para entrada de territórios OBS(cdt = cadastro)
 void cdt_Territorios(Territorio *territorios, int qtd) {
     for (int i = 0; i < qtd; i++) {
+
+        // Limpa a tela a cada novo território
+        system("clear"); // use "cls" se estiver no Windows
+
         printf ("=== Digite os dados do territorio %d ===\n\n", i + 1);
 
         printf ("Nome: ");
         fgets (territorios[i].nome, sizeof(territorios[i].nome), stdin);
-        territorios[i].nome[strcspn(territorios[i].nome, "\n")] = '\0';
+        territorios[i].nome[strcspn(territorios[i].nome, "\n")] = '\0'; // Limpa o \n(espaço, enter) do fgets
 
         printf ("Cor: ");
         fgets (territorios[i].cor, sizeof(territorios[i].cor), stdin);
-        territorios[i].cor[strcspn(territorios[i].cor, "\n")] = '\0';
+        territorios[i].cor[strcspn(territorios[i].cor, "\n")] = '\0'; // Limpa o \n(espaço, enter) do fgets
 
         printf ("Número de tropas: ");
         scanf ("%d", &territorios[i].tropas);
         getchar (); // Limpar o buffer
 
-        printf ("\n");
+        printf ("\n"); // Da um espaço para organização
 
     }
 }
 
+// Função para saída de dados dos territórios OBS(exib = exibir)
 void exib_Territorios(Territorio *territorios, int qtd) {
     printf ("=== Dados dos Territórios ===\n\n");
     for (int i = 0; i < qtd; i++) {
-    }
-}
-
-int main() {
-    int qtd_territorios = 5; // Definindo a quantidade de territorios
-
-    Territorio *territorios = (Territorio*) calloc(qtd_territorios, sizeof(Territorio)); // Alocando dinâmica de memória para os territórios
-
-    if (territorios == NULL) { // Programa encerra por erro de alocação
-        printf("Erro ao alocar memória!\n");
-        return 1;
-    }
-
-    // Preenchendo os dados dos territórios
-    for (int i = 0; i < qtd_territorios; i++) {
-        printf ("=== Digite os dados do território %d ===\n\n", i + 1);
-        
-        // Lendo o nome do território
-        printf ("Nome: ");
-        fgets (territorios[i].nome, sizeof(territorios[i].nome), stdin);
-        territorios[i].nome[strcspn(territorios[i].nome, "\n")] = '\0'; // Remover a nova linha do final
-        
-        // Lendo a cor do território
-        printf ("Cor: ");
-        fgets (territorios[i].cor, sizeof(territorios[i].cor), stdin);
-        territorios[i].cor[strcspn(territorios[i].cor, "\n")] = '\0'; // Remover a nova linha do final
-
-        // Lendo o número de tropas
-        printf ("Número de tropas: ");
-        scanf ("%d", &territorios[i].tropas);
-        getchar(); // Limpar o buffer do teclado
-
-        printf ("\n"); // Da um espaço para organização
-    }
-    
-    // Exibindo os dados dos territorios
-    printf ("=== Dados dos Territórios ===\n\n");
-    for (int i = 0; i < qtd_territorios; i++) {
-        printf ("%d. %s (%s) - %d Tropas\n",
+        printf ("%d. %s (%s, Tropas: %d)\n",
         i + 1,
         territorios[i].nome,
         territorios[i].cor,
         territorios[i].tropas);
 
     }
+}
 
+int main() {
+    int qtd_territorios = 5; // Definindo a quantidade de territorios
+
+    Territorio *territorios = (Territorio*) calloc(qtd_territorios, sizeof(Territorio)); // Alocação dinâmica de memória para os territórios
+
+    if (territorios == NULL) { // Programa encerra por erro de alocação
+        printf("Erro ao alocar memória!\n");
+        return 1;
+    }
+
+    // Chamada das funções
+    cdt_Territorios(territorios, qtd_territorios);
+    exib_Territorios(territorios, qtd_territorios);
+
+    // Liberando memória
     free(territorios);
 
     return 0;
